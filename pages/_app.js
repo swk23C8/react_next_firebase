c9fbefe0d71579973db5adc25842a2eb3e47418c
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2021-12-26 08:25:19 +0900

    finish app setup for development

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
 
+import Navbar from '../components/Navbar';
+import { Toaster } from 'react-hot-toast';
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Navbar />
+      <Component {...pageProps} />
+      <Toaster />
+    </>
+  );
 }
 
 export default MyApp
---
+++ pages/enter.js
@@ -0,0 +1,7 @@
+export default function EnterPage({ }) {
+  return (
+    <main>
+        <h1>Sign up</h1>
+    </main>
+  )
+}
