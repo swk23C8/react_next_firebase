1895f0736ce95bee23bb425256da286ba018b187
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2021-12-26 14:37:35 +0900

    fixing firebase v9 migration bugs

--- components/HeartButton.js
+++ components/HeartButton.js
@@ -1,36 +1,39 @@
-import { firestore, auth, increment } from '../lib/firebase';
+
+import { firestore, auth } from '../lib/firebase';
 import { useDocument } from 'react-firebase-hooks/firestore';
+import { increment, writeBatch, doc, getFirestore } from "firebase/firestore";
+
 
 // Allows user to heart or like a post
 export default function Heart({ postRef }) {
-    // Listen to heart document for currently logged in user
-    const heartRef = postRef.collection('hearts').doc(auth.currentUser.uid);
-    const [heartDoc] = useDocument(heartRef);
+   // Listen to heart document for currently logged in user
+   const heartRef = doc(getFirestore(), 'hearts', auth.currentUser.uid);
+   const [heartDoc] = useDocument(heartRef);
 
-    // Create a user-to-post relationship
-    const addHeart = async () => {
-        const uid = auth.currentUser.uid;
-        const batch = firestore.batch();
+   // Create a user-to-post relationship
+   const addHeart = async () => {
+      const uid = auth.currentUser.uid;
+      const batch = writeBatch(getFirestore());
 
-        batch.update(postRef, { heartCount: increment(1) });
-        batch.set(heartRef, { uid });
+      batch.update(postRef, { heartCount: increment(1) });
+      batch.set(heartRef, { uid });
 
-        await batch.commit();
-    };
+      await batch.commit();
+   };
 
-    // Remove a user-to-post relationship
-    const removeHeart = async () => {
-        const batch = firestore.batch();
+   // Remove a user-to-post relationship
+   const removeHeart = async () => {
+      const batch = writeBatch(getFirestore());
 
-        batch.update(postRef, { heartCount: increment(-1) });
-        batch.delete(heartRef);
+      batch.update(postRef, { heartCount: increment(-1) });
+      batch.delete(heartRef);
 
-        await batch.commit();
-    };
+      await batch.commit();
+   };
 
-    return heartDoc?.exists ? (
-        <button onClick={removeHeart}>💔 Unheart</button>
-    ) : (
-        <button onClick={addHeart}>💗 Heart</button>
-    );
+   return heartDoc?.exists ? (
+      <button onClick={removeHeart}>💔 Unheart</button>
+   ) : (
+      <button onClick={addHeart}>💗 Heart</button>
+   );
 }
