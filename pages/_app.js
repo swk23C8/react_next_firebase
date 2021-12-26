d2d2c59c482dda92bff4c2dc48af04a3a19f58e8
swk23C8 <35516289+swk23C8@users.noreply.github.com>
2021-12-26 12:14:06 +0900

    Implementing user Auth and Google sign-in

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,14 @@ import '../styles/globals.css'
 
 import Navbar from '../components/Navbar';
 import { Toaster } from 'react-hot-toast';
-
+import { UserContext } from '../lib/context'
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <UserContext.Provider value = {{ user: {}, username: 'testUser'}}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   );
 }
 
--- pages/enter.js
+++ pages/enter.js
@@ -1,7 +1,49 @@
-export default function EnterPage({ }) {
+import { auth, googleAuthProvider } from '../lib/firebase';
+import { signInWithPopup, signInAnonymously, signOut } from 'firebase/auth';
+import { useContext } from 'react';
+import { UserContext } from '../lib/context';
+
+
+export default function Enter(props) {
+  const {user, username } = useContext(UserContext);
+
+  // 1. user signed out <SignInButton />
+  // 2. user signed in, but missing username <UsernameForm />
+  // 3. user signed in, has username <SignOutButton />
   return (
     <main>
-        <h1>Sign up</h1>
+      {user ?
+        !username ? <UsernameForm /> : <SignOutButton />
+        :
+        <SignInButton />
+      }
     </main>
-  )
+  );
+}
+
+// Sign in with Google button
+function SignInButton() {
+  const signInWithGoogle = async () => {
+    await signInWithPopup(auth, googleAuthProvider)
+  };
+
+  return (
+    <>
+      <button className="btn-google" onClick={signInWithGoogle}>
+        <img src={'/google.png'} width="30px" /> Sign in with Google
+      </button>
+      <button onClick={() => signInAnonymously(auth)}>
+        Sign in Anonymously
+      </button>
+    </>
+  );
+}
+
+// Sign out button
+function SignOutButton() {
+  return <button onClick={() => signOut(auth)}>Sign Out</button>;
+}
+
+function UsernameForm() {
+  return null;
 }
